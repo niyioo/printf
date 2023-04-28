51217f951b9eaba2ef3108edac259529bf9be905
niyioo <niyiolagbegi@gmail.com>
2023-04-28 02:00:14 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,96 +1,38 @@
 #include "main.h"
 
 /**
- * _printf - Prints output according to a format.
- * @format: Format string containing directives.
- * Return: The number of characters printed (excluding the null byte).
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+	int printed_chars = 0, buffer_index = 0;
 
-	va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-	while (*format != '\0')
+	va_start(list, format);
+
+	while (*format)
 	{
-		if (*format == '%')
+		if (*format != '%')
 		{
-			format++; /* advance to the conversion specifier character */
-			switch (*format)
-			{
-				case 'c':
-					_putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					{
-						char *s = va_arg(args, char *);
-						while (*s != '\0')
-						{
-							_putchar(*s);
-							s++;
-							count++;
-						}
-						break;
-					}
-				case '%':
-					_putchar('%');
-					count++;
-					break;
-				case 'd':
-				case 'i':
-					print_number(va_arg(args, int));
-					count++;
-					break;
-				default:
-					break;
-			}
+			buffer[buffer_index++] = *format++;
+			if (buffer_index == BUFF_SIZE)
+				printed_chars += flush_buffer(buffer, &buffer_index);
 		}
 		else
 		{
-			_putchar(*format);
-			count++;
+			format++;
+			buffer_index += handle_specifier(format, &list, buffer, &buffer_index);
+			format += get_specifier_length(format);
 		}
-		format++;
 	}
 
-	va_end(args);
-
-	return (count);
-}
-
-/**
- * _putchar - Writes a character to stdout.
- * @c: The character to write.
- */
-void _putchar(char c)
-{
-	putchar(c);
-}
-
-/**
- * print_number - Prints an integer.
- * @n: The integer to print.
- */
-void print_number(int n)
-{
-	unsigned int n_abs;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n_abs = -n;
-	}
-	else
-	{
-		n_abs = n;
-	}
-
-	if (n_abs / 10)
-	{
-		print_number(n_abs / 10);
-	}
-
-	_putchar((n_abs % 10) + '0');
+	printed_chars += flush_buffer(buffer, &buffer_index);
+	va_end(list);
+	return (printed_chars);
 }
